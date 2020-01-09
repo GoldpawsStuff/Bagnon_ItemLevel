fb22f42a7e7dbe359c6ce9d9321f062c3e09bada
Lars Norberg <gitkraken@larsnorbergofficial.com>
2020-01-09 09:20:45 +0100

    1.0.29-Release

--- Bagnon_ItemLevel/main.lua
+++ Bagnon_ItemLevel/main.lua
@@ -206,6 +206,4 @@ local Update = function(self)
 	end	
 end 
 
-Module.OnEnable = function(self)
-	hooksecurefunc(Bagnon.ItemSlot, "Update", Update)
-end 
+hooksecurefunc(Bagnon.ItemSlot, "Update", Update)
